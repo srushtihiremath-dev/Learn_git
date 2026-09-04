a5bbbd5cbc42fa1be5c4d2723b33906a459e8546
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-04 10:18:07 +0530

    update

---
+++ Addition.java
@@ -0,0 +1,11 @@
+public class AAddition {
+    public static void main(String[] args){
+        int a = 123;
+        int b = 4;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+        
+    }
+}
