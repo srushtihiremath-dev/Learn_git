462586729e6a9ca582773066a767883f88f3c2fa
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-04 10:24:52 +0530

    Second commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    static void myMethod() {
+            System.out.println("Learning git");
+        }
+    public static void main(String[] args){
+       myMethod();
+    }
+}
