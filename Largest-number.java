bea29d74dadc5821ac836bdb6047bab57b01c079
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-04 10:12:51 +0530

    First commit

---
+++ Largest-number.java
@@ -0,0 +1,23 @@
+public class Largest-number {
+    public static void main(String[] args) {
+        int a = 10, b = 8, c = 2;
+
+        if (a > b)
+        {
+            System.out.println("a is greater than b");
+        }
+        else if (a > c)
+        {
+            System.out.println("a is greater than c");
+        
+        }
+        else if (b > c)
+        {
+            System.out.println("b is greater than c");
+        
+        }
+        else {
+            System.out.println("b is less than c");
+        }   
+    }
+}
