b04b9fb871cb8a683e578c582a695973bca0c8e9
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-07 11:17:42 +0530

    Good Morning

--- modules/find_by_title.py
+++ modules/find_by_title.py
@@ -5,7 +5,3 @@ def find_by_title(db_path, title):
             Returns genres and keywords for a movie by title from the SQLite database.
         '''
 
-        except sqlite3.Error as error:
-            print(f"❌SQLite error: ", error)
-            return 
-
