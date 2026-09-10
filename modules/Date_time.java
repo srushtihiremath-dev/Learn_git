8df2feb33cd2af26fe99ad54050a37485d0fe4f8
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-10 10:32:05 +0530

    Date and time

---
+++ modules/Date_time.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+public class Date_time {
+    public static void main(String[] args) {
+
+        LocalDateTime date = LocalDateTime.now();
+        system.out.println(date);
+
+    }
+}
