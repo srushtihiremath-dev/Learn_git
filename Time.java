bb0b9fdeaa48f69bd079098b99ebbfc171a2c934
srushtihiremath-dev <asrushti113@gmail.com>
2026-09-10 10:45:08 +0530

    Times java

---
+++ Time.java
@@ -0,0 +1,8 @@
+import java.time.LocalTime;
+
+public class Times{
+    public static void main(String[] args) {
+        LocalTime td1 =  LocalTime.now();
+        System.out.println(td1);
+    }
+}
